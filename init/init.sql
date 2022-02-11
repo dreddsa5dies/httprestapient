a7013ca59e64787fc4d194e39548d5541ee7cd7a
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-02-11 12:51:36 +0300

    postgresql tests +

---
+++ init/init.sql
@@ -0,0 +1,3 @@
+CREATE TABLE vendors (Id TEXT UNIQUE PRIMARY KEY, VendorName TEXT, NameMatrix TEXT, VersionMatrix TEXT, CreateDate DATE, ModifyDate DATE);
+INSERT INTO vendors VALUES ('G0138', 'Andariel', 'Silent Chollima', '1.0', '29 September 2021', '15 October 2021');
+INSERT INTO vendors VALUES ('T1595.001', 'PRE', 'Active Scanning: Scanning IP Blocks', '2.0', '02 October 2020', '15 April 2021');
