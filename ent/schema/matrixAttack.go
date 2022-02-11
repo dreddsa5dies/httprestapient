fdc1543162ab6cfd47fbab1da2fb227967c1421c
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-02-11 14:07:29 +0300

    add MatrixAttack schema

---
+++ ent/schema/matrixAttack.go
@@ -0,0 +1,25 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the MatrixAttack entity.
+type MatrixAttack struct {
+	ent.Schema
+}
+
+// Fields of the MatrixAttack.
+func (MatrixAttack) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("Id").Unique().Default("unknown"),
+		field.String("VendorName").Default("unknown"),
+		field.String("NameMatrix").Default("unknown"),
+		field.String("VersionMatrix").Default("unknown"),
+		field.Time("CreateDate").Default(time.Now),
+		field.Time("ModifyDate").Default(time.Now),
+	}
+}
